16fc52944c16d2ee8de08034b9dc0aac638d4d0c
Parker Lougheed <parlough@gmail.com>
2022-12-15 05:22:22 -0600

    Use new generation system to identify active items in the sidenav
    This allows for items to be located in other places, but highlight the more specific location.

---
+++ src/_plugins/active_nav_for_page.rb
@@ -0,0 +1,31 @@
+module ActiveNavForPage
+  def active_nav_for_page(page_url_path = '', active_nav)
+    parts = page_url_path.downcase.split('/').drop(1)
+    current_path_pairs = active_nav
+    last_allowed_backup_active = []
+
+    parts.each do |part|
+      paths = current_path_pairs['paths']
+      unless paths.nil?
+        current_path_pairs = paths
+      end
+      next_pair = current_path_pairs[part]
+
+      # If the last entry allowed children and has active data
+      # allow it to be a backup if a path is not found
+      unless current_path_pairs['active'].nil? or current_path_pairs['allow-children'] == false
+        last_allowed_backup_active = current_path_pairs
+      end
+
+      # If the next part of the path does not exist
+      if next_pair.nil?
+        return last_allowed_backup_active
+      end
+      current_path_pairs = next_pair
+    end
+
+    return current_path_pairs['active']
+  end
+end
+
+Liquid::Template.register_filter(ActiveNavForPage)
